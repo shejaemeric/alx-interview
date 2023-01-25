c389e853a1636a649497f4a9c09ecdc6a1228f1a
shejaemeric2 <shejaemeric2@gmail.com>
2023-01-25 09:49:51 +0200

    created 0-pasclal_triangle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""module to display pascal triangle"""
+
+
+def pascal_triangle(n):
+
+    """display pascal triangle of n
+
+    Args:
+        n (integer): pascal number to stop on.
+    """
+
+    if n <= 0:
+        return []
+    else:
+        results = []
+        pascal_list = [1, 1]
+        for n in range(2, n):
+            new_pascal_list = [1]
+            for n in range(0, len(pascal_list) - 1):
+                new_pascal_list.append(
+                    pascal_list[n] + pascal_list[n+1])
+            new_pascal_list.append(1)
+            pascal_list = new_pascal_list
+            results.append(pascal_list)
+        return results
