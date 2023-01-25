c2c3846096dbce33177bfc0387f26e33a59c98b0
shejaemeric2 <shejaemeric2@gmail.com>
2023-01-25 10:04:29 +0200

    fix error

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -12,8 +12,10 @@ def pascal_triangle(n):
 
     if n <= 0:
         return []
+    elif (n == 1):
+        return [[1],[1,1]]
     else:
-        results = []
+        results = [[1],[1,1]]
         pascal_list = [1, 1]
         for n in range(2, n):
             new_pascal_list = [1]
--- README.md
+++
@@ -1,3 +0,0 @@
-## ALX INTERVIEW PROJECT
-Project by ALX
-author: SHEJA Lervy Emeric
